83a884c0d646120a285524f61759b59b8f521479
Berkay Karakoc <Berkay Karakoc@192.168.1.4>
2019-01-08 20:37:26 +0300

    Added some comments.

--- G12_CENG431_HW5/src/business/Computer.java
+++ G12_CENG431_HW5/src/business/Computer.java
@@ -2,10 +2,9 @@ package business;
 
 public interface Computer {
 
+	//decorator methods
 	void plugCable();
-	
 	void add2Price(int price);
-	
 	void setCableType(CableType type);
 	
 }
--- G12_CENG431_HW5/src/business/Demo.java
+++ G12_CENG431_HW5/src/business/Demo.java
@@ -4,17 +4,21 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
+		//create factories 
 		ComputerFactory gamingFactory = new GamingComputerFactory();
 		ComputerFactory regularFactory = new RegularComputerFactory();
 		
+		//give factories to pc assembler
 		PCAssembler gamingPC = new PCAssembler(gamingFactory);
 		PCAssembler regularPC = new PCAssembler(regularFactory);
 		
+		//assemble a gaming pc and print specifications
 		System.out.println("Gaming Computer:");
 		System.out.println(gamingPC.assembleComputer());
 		
 		System.out.println();
 		
+		//assemble a gold cabled gaming pc and print specifications
 		System.out.println("Gold Cabled Gaming Computer:");
 		Computer goldGaming = new GoldCabledComputer(gamingPC.assembleComputer());
 		goldGaming.plugCable();
@@ -22,6 +26,7 @@ public class Demo {
 		
 		System.out.println();
 		
+		//assemble a silver cabled gaming pc and print specifications
 		System.out.println("Silver Cabled Gaming Computer:");
 		Computer silverGaming = new SilverCabledComputer(gamingPC.assembleComputer());
 		silverGaming.plugCable();
@@ -29,11 +34,13 @@ public class Demo {
 		
 		System.out.println();
 		
+		//assemble a regular pc and print specifications
 		System.out.println("Regular Computer:");
 		System.out.println(regularPC.assembleComputer());
 		
 		System.out.println();
 		
+		//assemble a gold cabled regular pc and print specifications
 		System.out.println("Gold Cabled Regular Computer:");
 		Computer goldRegular = new GoldCabledComputer(regularPC.assembleComputer());
 		goldRegular.plugCable();
@@ -41,6 +48,7 @@ public class Demo {
 		
 		System.out.println();
 		
+		//assemble a silver cabled regular pc and print specifications
 		System.out.println("Silver Cabled Regular Computer:");
 		Computer silverRegular = new SilverCabledComputer(regularPC.assembleComputer());
 		silverRegular.plugCable();
